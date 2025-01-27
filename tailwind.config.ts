a9abe4c70a5b0eb1cc8d610b9aee89eb8b5aceed
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-01-27 18:45:02 +0700

    feat: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        primary: '#5784B2',
+        secondary: '#D5E1E9',
+        black_ani: '#252525',
+      },
+      backgroundColor: {
+        primary: '#5784B2',
+        secondary: '#D5E1E9',
+        black_ani: '#252525',
       },
     },
   },
