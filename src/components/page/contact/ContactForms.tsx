c0b6fd7c99816cd43f8103704bb32bcf387d9ac1
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-02-14 20:01:30 +0700

    feat: consum the sender api for contact

--- src/components/page/contact/ContactForms.tsx
+++ src/components/page/contact/ContactForms.tsx
@@ -1,127 +1,202 @@
 'use client';
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
-import { useForm } from "react-hook-form";
-import { BiSolidCategory } from "react-icons/bi";
-import { BsPersonFill } from "react-icons/bs";
-import { MdEmail, MdCall } from "react-icons/md";
-import { TbMessageFilled } from "react-icons/tb";
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import { useForm } from 'react-hook-form';
+import { BiSolidCategory } from 'react-icons/bi';
+import { BsPersonFill } from 'react-icons/bs';
+import { MdEmail, MdCall } from 'react-icons/md';
+import { TbMessageFilled } from 'react-icons/tb';
+import { useState } from 'react';
+import axios from 'axios';
 
 const ContactForms = () => {
-    const form = useForm({
-        defaultValues: {
-            name: "",
-            email: "",
-            phone: "",
-            subject: "",
-            message: ""
-        }
-    });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [response, setResponse] = useState<string>('');
+  const form = useForm({
+    defaultValues: {
+      name: '',
+      email: '',
+      phone: '',
+      subject: '',
+      message: '',
+    },
+  });
 
-    const onSubmit = (values: unknown) => {
-        console.log(values);
-    };
+  const onSubmit = async (values: unknown) => {
+    console.log(values);
+    setIsLoading(true);
 
-    return (
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 max-w-lg mx-auto">
-                <FormField
-                    control={form.control}
-                    name="name"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="pl-[10px]">Nama</FormLabel>
-                            <FormControl>
-                                <div className="relative">
-                                    <BsPersonFill className="absolute left-3 top-1/2 -translate-y-1/2 text-title" size={18} />
-                                    <Input {...field} placeholder="masukkan nama" className="pl-10" />
-                                </div>
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel className="pl-[10px]">Email</FormLabel>
-                                <FormControl>
-                                    <div className="relative">
-                                        <MdEmail className="absolute left-3 top-1/2 -translate-y-1/2 text-title" size={18} />
-                                        <Input {...field} placeholder="masukkan email" className="pl-10" type="email"/>
-                                    </div>
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="phone"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel className="pl-[10px]">Nomor Telepon</FormLabel>
-                                <FormControl>
-                                    <div className="relative">
-                                        <MdCall className="absolute left-3 top-1/2 -translate-y-1/2 text-title" size={18} />
-                                        <Input {...field} placeholder="masukkan nomor telepon" className="pl-10" type="number"/>
-                                    </div>
-                                </FormControl>
-                            </FormItem>
-                        )}
-                    />
+    try {
+      const res = axios.post('/api/send', values);
+      setResponse('Pesanmu Berhasil Dikirim');
+      console.log('Ini hasil post', res);
+    } catch (error: any) {
+      console.error('Error:', error.response?.data || error.message);
+      setResponse('Gagal mengirim pesan');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='space-y-4 max-w-lg mx-auto'
+      >
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='pl-[10px]'>Nama</FormLabel>
+              <FormControl>
+                <div className='relative'>
+                  <BsPersonFill
+                    className='absolute left-3 top-1/2 -translate-y-1/2 text-title'
+                    size={18}
+                  />
+                  <Input
+                    {...field}
+                    placeholder='masukkan nama'
+                    className='pl-10'
+                  />
                 </div>
-                <FormField
-                    control={form.control}
-                    name="subject"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="pl-[10px]">Subjek/Topik</FormLabel>
-                            <FormControl>
-                                <Select onValueChange={field.onChange} defaultValue={field.value}>
-                                    <SelectTrigger className="flex justify-between">
-                                        <div className="flex justify-start gap-2">
-                                            <BiSolidCategory className="text-title" size={18} />
-                                            <SelectValue placeholder="pilih subjek/topik pesan" className="text-muted-foreground" />
-                                        </div>
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="support">Dukungan</SelectItem>
-                                        <SelectItem value="sales">Penawaran</SelectItem>
-                                        <SelectItem value="other">Lainnya</SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
-                <FormField
-                    control={form.control}
-                    name="message"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel className="pl-[10px]">Pesan</FormLabel>
-                            <FormControl>
-                                <div className="relative">
-                                    <TbMessageFilled className="absolute left-3 top-4 text-title" size={18} />
-                                    <Textarea {...field} placeholder="masukkan pesan" className="pl-10 pt-3" rows={4} />
-                                </div>
-                            </FormControl>
-                        </FormItem>
-                    )}
-                />
-                <div className="w-full flex items-center justify-center">
-                    <Button type="submit" className="w-[80%] h-12 bg-primary_ani text-white">Kirim</Button>
-
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
+          <FormField
+            control={form.control}
+            name='email'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='pl-[10px]'>Email</FormLabel>
+                <FormControl>
+                  <div className='relative'>
+                    <MdEmail
+                      className='absolute left-3 top-1/2 -translate-y-1/2 text-title'
+                      size={18}
+                    />
+                    <Input
+                      {...field}
+                      placeholder='masukkan email'
+                      className='pl-10'
+                      type='email'
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name='phone'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className='pl-[10px]'>Nomor Telepon</FormLabel>
+                <FormControl>
+                  <div className='relative'>
+                    <MdCall
+                      className='absolute left-3 top-1/2 -translate-y-1/2 text-title'
+                      size={18}
+                    />
+                    <Input
+                      {...field}
+                      placeholder='masukkan nomor telepon'
+                      className='pl-10'
+                      type='number'
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name='subject'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='pl-[10px]'>Subjek/Topik</FormLabel>
+              <FormControl>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <SelectTrigger className='flex justify-between'>
+                    <div className='flex justify-start gap-2'>
+                      <BiSolidCategory
+                        className='text-title'
+                        size={18}
+                      />
+                      <SelectValue
+                        placeholder='pilih subjek/topik pesan'
+                        className='text-muted-foreground'
+                      />
+                    </div>
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value='support'>Dukungan</SelectItem>
+                    <SelectItem value='sales'>Penawaran</SelectItem>
+                    <SelectItem value='other'>Lainnya</SelectItem>
+                  </SelectContent>
+                </Select>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='message'
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='pl-[10px]'>Pesan</FormLabel>
+              <FormControl>
+                <div className='relative'>
+                  <TbMessageFilled
+                    className='absolute left-3 top-4 text-title'
+                    size={18}
+                  />
+                  <Textarea
+                    {...field}
+                    placeholder='masukkan pesan'
+                    className='pl-10 pt-3'
+                    rows={4}
+                  />
                 </div>
-            </form>
-        </Form>
-    );
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <div className='w-full flex items-center justify-center'>
+          <Button
+            type='submit'
+            className='w-[80%] h-12 bg-primary_ani text-white'
+          >
+            Kirim
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default ContactForms;
