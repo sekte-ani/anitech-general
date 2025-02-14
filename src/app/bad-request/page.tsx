9e8c17cf44fac01a5a19a28fccaa4dba78936e53
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-02-14 20:36:25 +0700

    feat: make 400 and 404 oage

--- src/app/bad-request/page.tsx
+++ src/app/bad-request/page.tsx
@@ -3,17 +3,16 @@
 import Description from '@/components/micro/Description';
 import ImageComponent from '@/components/micro/ImageComponent';
 import SectionTitle from '@/components/micro/SectionTitle';
-import { useRouter } from 'next/navigation';
 
-export default function NotFound() {
+export default function BadRequest() {
   return (
     <div className='flex flex-col gap-10 py-40 px-12 text-center justify-center items-center'>
       <ImageComponent
-        src='/img/404.png'
-        alt='Not Found'
+        src='/img/400.png'
+        alt='Bad Request'
       />
-      <SectionTitle title='Oops, Halaman Tidak Ditemukan' />
-      <Description text='Sepertinya halaman yang kamu cari tidak tersedia.' />
+      <SectionTitle title='Oops, Halaman Bermasalah' />
+      <Description text='Permitaan yang dicari tidak tersedia' />
     </div>
   );
 }
