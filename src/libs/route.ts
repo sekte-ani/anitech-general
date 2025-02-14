eec0e17993292247d57370d9cf12a252af1b3c53
Ferdiyansyah59 <ferdir5599@gmail.com>
2025-02-14 19:20:56 +0700

    refactor: change responsive employee division from list to dropdown menu

---
+++ src/libs/route.ts
@@ -0,0 +1,38 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  try {
+    const { name, subject, email, message } = await req.json();
+
+    if (!name || !subject || !email || !message) {
+      return new Response(
+        JSON.stringify({ error: 'All fields are required' }),
+        {
+          status: 400,
+        },
+      );
+    }
+
+    const emailResponse = await resend.emails.send({
+      from: 'onboarding@resend.dev', // Ganti dengan domain verifikasi dari Resend
+      to: process.env.MY_EMAIL!,
+      subject: `Pesan dari Web A.N.I: ${subject}`,
+      html: `<p><strong>Name:</strong> ${name}</p>
+             <p><strong>Email:</strong> ${email}</p>
+             <p><strong>Message:</strong> ${message}</p>`,
+    });
+
+    return new Response(
+      JSON.stringify({ success: true, data: emailResponse }),
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    return new Response(JSON.stringify({ success: false, error }), {
+      status: 500,
+    });
+  }
+}
