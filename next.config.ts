6e01f491f04e7ce17676bcd97672cdb3fb829131
Gswaraw37 <genta.swara@gmail.com>
2025-02-15 14:35:21 +0700

    feat: Menambahkan Dockerfile, docker-compose, setup nginx

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['82.112.230.106'],
+    domains: ["82.112.230.106"],
   },
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/structure",
+        destination: "https://82.112.230.106:8009/api/structure",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
