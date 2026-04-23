4967ef62eecc694e09faeaaf8193b928e893372c
Ferdiyansyah59 <ferdir5599@gmail.com>
2026-04-23 09:15:34 +0700

    refactor: next js version updated

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["82.112.230.106"],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
